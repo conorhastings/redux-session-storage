0fae4686563175a506274c196c1fabc0d1f5b74b
conorhastings <hastings.conorm@gmail.com>
2015-11-20 14:51:19 -0500

    expose getState in action transform

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import {get, set, remove} from "./helpers/session-storage";
 
 export default function actionRecorder({key, transform, limit, reset}) {
 	let callCount = 0;
-	return () => (next) => (action) => {
+	return (getState) => (next) => (action) => {
 		if (reset && !callCount) {
 			remove({key});
 		}
 		callCount += 1;
 		let actions = [];
-		const transformedAction = transform ? transform(action) : action;
+		const transformedAction = transform ? transform(action, getState) : action;
 		let actionToStore = Object.keys(transformedAction).reduce((actionData, item) => {
 			actionData[item] = transformedAction[item];
 			return actionData;
